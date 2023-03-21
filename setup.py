d27b57e42c25c77fe0d9c02725ea805431fce72c
RemyCocq <remy.cocquempot@leroymerlin.fr>
2023-03-21 22:25:38 +0100

    build: init package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='flowcard',
+    packages=['flowcard'],
+    version='0.0.1',
+    license='MIT',
+    description='TYPE YOUR DESCRIPTION HERE',
+    author='RemyCocq',
+    url='https://github.com/RemyCocq/flowcard',
+    download_url='https://github.com/RemyCocq/flowcard/archive/v_01.tar.gz',
+    keywords=['card', 'html', 'markdown'],
+    install_requires=[]
+)
