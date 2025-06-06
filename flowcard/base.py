4f47b87319b15c1dd50c3543c9cb7877d45323a5
RemyCocq <remy.cocquempot@leroymerlin.fr>
2025-06-06 22:50:44 +0200

    feat(image): make example with image work

--- flowcard/base.py
+++ flowcard/base.py
@@ -16,48 +16,48 @@ class Title(Component):
     name = "title"
     aliases = ["header"]
 
-    def __init__(self, text):
+    def __init__(self, text: str) -> None:
         super().__init__()
         self.text = text
 
-    def to_html(self):
+    def to_html(self) -> dict[str, str]:
         return {"head": f"<title>{self.text}</title>", "body": f"<h1>{self.text}</h1>"}
 
-    def to_markdown(self):
+    def to_markdown(self) -> str:
         return f"# {self.text}"
 
 
 class Favicon(Component):
     name = "favicon"
 
-    def __init__(self, image_data):
+    def __init__(self, image_data: bytes) -> None:
         super().__init__()
-        self.base64 = b64encode(image_data).decode("ascii")
-        self.mime = m.id_buffer(image_data)
+        self.base64 = b64encode(image_data).decode(encoding="ascii")
+        self.mime = m.from_buffer(image_data)
 
-    def to_html(self):
+    def to_html(self) -> dict[str, str]:
         return {
             "head": f"<link rel='icon' type='{self.mime}'  href='data:{self.mime};base64,{self.base64}'/>",
             "body": "",
         }
 
-    def to_markdown(self):
+    def to_markdown(self) -> str:
         return ""
 
 
 class Image(Component):
     name = "image"
 
-    def __init__(self, image_data, width=None, height=None):
+    def __init__(self, image_data: bytes, width: int | None = None, height: int | None = None) -> None:
         super().__init__()
-        self.base64 = b64encode(image_data).decode("ascii")
-        self.mime = m.id_buffer(image_data)
+        self.base64 = b64encode(image_data).decode(encoding="ascii")
+        self.mime = m.from_buffer(image_data)
 
-    def to_html(self):
+    def to_html(self) -> dict[str, str]:
         return {
             "head": "",
             "body": f"<img type='{self.mime}'  src='data:{self.mime};base64,{self.base64}'/>",
         }
 
-    def to_markdown(self):
+    def to_markdown(self) -> str:
         return f"<img type='{self.mime}'  src='data:{self.mime};base64,{self.base64}'/>"
